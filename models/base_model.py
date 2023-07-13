c453239f8e53b0b284496cbc61623c1056683e0d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-07-13 23:28:24 +0100

    AirBnB Clone 4

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,22 @@ from datetime import datetime
 import uuid
 
 class BaseModel:
-    """BaseModel class
-    """
+    """BaseModel class"""
     
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key == '__class__':
+                    continue
+                else:
+                    setattr(self, key, val)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
         
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
